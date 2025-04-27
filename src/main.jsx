bda31e596f2a0404d496b732feba8cb263ce79fd
Saidam Anil Kumar <anilkumarakvlcss6616@gmail.com>
2025-04-27 13:15:25 +0530

    create pages and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// Debugging check
+console.log('Mounting point exists:', !!document.getElementById('root'))
+
+const root = createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
