c998b515d110f4d87078302d7968f3e2bdcc06f4
Saidam Anil Kumar <anilkumarakvlcss6616@gmail.com>
2025-05-03 18:02:34 +0530

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
 
-// Debugging check
-console.log('Mounting point exists:', !!document.getElementById('root'))
-
-const root = createRoot(document.getElementById('root'))
-root.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>
-)
+  </Provider>
+);
